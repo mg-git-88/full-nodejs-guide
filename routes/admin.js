c95f529996a7536cd86cc9bd907c8fd39572d7d8
mg-git-88 <mayukhganguly88@gmail.com>
2020-01-19 02:10:57 +0530

    Using Templating Engine Express-Handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,10 @@ const products = [];
 router.get("/add-product", (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add New Product",
-    path: "/admin/add-product"
+    path: "/admin/add-product",
+    activeProduct: true,
+    formsCSS: true,
+    productCSS: true
   });
 });
 
