2570d170555cce9acdb45e766a34b5e3208423e3
mg-git-88 <mayukhganguly88@gmail.com>
2020-01-22 22:35:27 +0530

    Added Model

--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,24 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAllProducts = (req, res, next) => {
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/"
+  Product.fetchAll(products => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "Products",
+      path: "/product-list"
+    });
   });
 };
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add New Product",
     path: "/admin/add-product"
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
---
+++ models/product.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+
+const fileLoc = require("../util/path");
+
+const filePath = path.join(fileLoc, "data", "products.json");
+
+const getProductsFromFile = cb => {
+  fs.readFile(filePath, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(filePath, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+}
